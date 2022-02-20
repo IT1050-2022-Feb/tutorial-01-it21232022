6da2b248b1eec7861cdd70e8951f2d043485cec7
it21232022 <99676012+it21232022@users.noreply.github.com>
2022-02-20 17:17:17 +0000

    complete the code

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,15 @@
 #include <stdio.h>
 
 int main() {
+  int num1, num2;
+  float sum, avg;
+  printf("Enter the mark 1st subject :");
+  scanf("%d", &num1);
+  printf("Enter the mark 2nd subject :");
+  scanf("%d", &num2);
+  sum = num1 + num2;
+  avg = sum/2;
+  printf("Average of two subjects = %.1f\n", avg);
   
   return 0;
 }
--- Tute02.c
+++ Tute02.c
@@ -17,8 +17,21 @@ Distance -> 50
 Amount = 30 x 50 + (50-30) x 40 = 2300*/
 
 #include <stdio.h>
-
 int main() {
+float distance, amount;
+
+printf("Enter the distance that van has travelled :");
+scanf("%f", &distance);
+
+if(distance <= 30)
+{
+  amount = distance*50;
+}
+else if(distance > 30)
+{
+  amount = 30*50 + (distance - 30)*40;
+}
+printf("Amount to be paid = %.2f\n", amount);
   
   return 0;
 }
